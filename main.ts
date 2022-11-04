85f390bd84697f99ff87a1203963b8ce78633910
RetiredLake9230 <nicodinicola@hotmail.com>
2022-11-04 11:22:42 -0700

    Better pairing

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (ex == true) {
+    wed = receivedNumber
+    if (receivedNumber == 5) {
+        basic.showString("Get Ready")
+        basic.pause(5000)
+    } else if (receivedNumber == wed) {
         basic.showLeds(`
             . . . . #
             . . . # .
@@ -7,13 +11,6 @@ radio.onReceivedNumber(function (receivedNumber) {
             . # . . .
             . . . . .
             `)
-        ex = false
-        wed = receivedNumber
-    } else if (receivedNumber == 5) {
-        basic.showString("Get Ready")
-        basic.pause(5000)
-    } else if (false) {
-    	
     } else {
         basic.showLeds(`
             # . . . #
@@ -30,11 +27,11 @@ input.onButtonPressed(Button.AB, function () {
     basic.pause(5000)
 })
 let wed = 0
-let ex = false
 let id = control.deviceSerialNumber()
-loops.everyInterval(1000, function () {
+for (let index = 0; index < 6; index++) {
     radio.sendNumber(id)
-})
+    basic.pause(1000)
+}
 basic.forever(function () {
-    ex = true
+	
 })
